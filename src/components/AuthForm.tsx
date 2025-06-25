64dab241a412f28283aa14bca3ee8cc3bc5f6c70
cecilia314 <lopezsisternacecilia@yahoo.com>
2025-06-25 00:23:15 -0700

    feat: Create OTP modal

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { Input } from '@/components/ui/input';
 import Image from 'next/image';
 import Link from 'next/link';
 import { createAccount } from '@/lib/actions/user.actions';
+import OTPModal from './OTPModal';
 
 type FormType = 'sign-in' | 'sign-up';
 
@@ -147,7 +148,11 @@ const AuthForm = ({ type }: { type: FormType }) => {
           </div>
         </form>
       </Form>
+
       {/* OTP Verification */}
+      {accountId && (
+        <OTPModal email={form.getValues('email')} accountId={accountId} />
+      )}
     </>
   );
 };
