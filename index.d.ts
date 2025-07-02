716f3a1375ca818f323f4728a13a588a2e0de40c
cecilia314 <lopezsisternacecilia@yahoo.com>
2025-07-02 13:43:54 -0700

    feat: Create dropdown menu card

--- index.d.ts
+++ index.d.ts
@@ -40,3 +40,26 @@ declare interface GetFilesProps {
   sort?: string;
   limit?: number;
 }
+
+declare interface ActionType {
+  label: string;
+  icon: string;
+  value: string;
+}
+
+declare interface RenameFileProps {
+  fileId: string;
+  name: string;
+  extension: string;
+  path: string;
+}
+declare interface UpdateFileUsersProps {
+  fileId: string;
+  emails: string[];
+  path: string;
+}
+declare interface DeleteFileProps {
+  fileId: string;
+  bucketFileId: string;
+  path: string;
+}
